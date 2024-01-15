ecf16b9ff1a15eb274dc4c2bfd71c55587d9d6b8
mapadilha <devpadilha@gmail.com>
2024-01-15 14:32:49 -0300

    salvando e recuperando clientes

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,10 +1,28 @@
 package org.example;
 
+import org.example.domain.entity.Cliente;
+import org.example.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+           clientes.salvar(new Cliente("Padilha"));
+           clientes.salvar(new Cliente("Michelle"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
