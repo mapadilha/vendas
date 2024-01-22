e875bea471222bce84eb6c24bf02fef6ecde9a76
mapadilha <devpadilha@gmail.com>
2024-01-22 10:55:24 -0300

    criando reposÃitório das entidades

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.example.domain.entity.Cliente;
-import org.example.domain.repositorio.Clientes;
+import org.example.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
