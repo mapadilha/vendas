8042f37f93b89495f1a6b2bd651708d405ebc1c0
mapadilha <devpadilha@gmail.com>
2024-01-22 10:17:03 -0300

    mapeando a classe pedido

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package org.example.domain.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "cliente")
@@ -12,8 +13,18 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @OneToMany (mappedBy = "cliente")
+    private Set<Pedido> pedidos;
     public Cliente() {
 
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 
     public Cliente(Integer id, String nome) {
--- src/main/java/org/example/domain/entity/Pedido.java
+++ src/main/java/org/example/domain/entity/Pedido.java
@@ -1,12 +1,22 @@
 package org.example.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+    @Column(name = "data_pedido")
     private LocalDate dataPedido;
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 
     public Integer getId() {
