de575672f2dce0a6a7aaf72792cd5d6fa3f4aee0
mapadilha <devpadilha@gmail.com>
2024-01-24 15:21:33 -0300

    Requisição GET com parâmentros e Response Entity

--- src/main/java/org/example/rest/controller/ClienteController.java
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -1,21 +1,31 @@
 package org.example.rest.controller;
 
+import org.example.domain.entity.Cliente;
+import org.example.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
 
-    @RequestMapping(
-            value = {"/api/clientes/hello/{nome}"},
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"}
-    )
-    public String helloCliente (@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s", nomeCliente);
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+            return ResponseEntity.notFound().build();
     }
 
 }
