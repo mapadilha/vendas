dac09d2b39607dfb100f4900447b61b87b3fcd16
mapadilha <devpadilha@gmail.com>
2024-01-24 14:55:55 -0300

    mapeando as requisicoes com requesting mapping

--- src/main/java/org/example/rest/controller/ClienteController.java
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}"},
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"},
+            produces = {"application/json", "application/xml"}
+    )
     public String helloCliente (@PathVariable("nome") String nomeCliente) {
         return String.format("Hello %s", nomeCliente);
     }
