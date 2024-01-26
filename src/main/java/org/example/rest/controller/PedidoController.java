ef8d0d373ec70b8e0b73120e5adfd9eca18ffcc8
mapadilha <devpadilha@gmail.com>
2024-01-26 13:06:13 -0300

    Obtendo os detalhes de um pedido

--- src/main/java/org/example/rest/controller/PedidoController.java
+++ src/main/java/org/example/rest/controller/PedidoController.java
@@ -1,10 +1,20 @@
 package org.example.rest.controller;
 
+import org.example.domain.entity.ItemPedido;
 import org.example.domain.entity.Pedido;
+import org.example.domain.repository.Pedidos;
+import org.example.rest.dto.InformacaoItemPedidoDTO;
+import org.example.rest.dto.InformacoesPedidoDTO;
 import org.example.rest.dto.PedidoDTO;
 import org.example.service.PedidoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -23,5 +33,27 @@ public class PedidoController {
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
     }
-
+    private InformacoesPedidoDTO converter (Pedido pedido) {
+        return InformacoesPedidoDTO
+                .builder()
+                .codigo(pedido.getId())
+                .dataPedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+                .items(converter(pedido.getItens()))
+                .build();
+    }
+    private List<InformacaoItemPedidoDTO> converter(List<ItemPedido> itens) {
+        if(CollectionUtils.isEmpty(itens)) {
+            return Collections.emptyList();
+        }
+        return itens.stream().map(
+                item -> InformacaoItemPedidoDTO
+                                .builder().descricaoProduto(item.getProduto().getDescricao())
+                                .precoUnitario(item.getProduto().getPreco())
+                                .quantidade(item.getQuantidade())
+                                .build()
+        ).collect(Collectors.toList());
+    }
 }
