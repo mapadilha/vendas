2809c0725988e9dd51d9e0ced41ba98d5e2242f2
mapadilha <devpadilha@gmail.com>
2024-01-29 14:45:20 -0300

    Utilizando e Testando o Bean Validation

--- src/main/java/org/example/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/org/example/rest/controller/ApplicationControllerAdvice.java
@@ -4,10 +4,14 @@ import org.example.exception.PedidoNaoEncontradoException;
 import org.example.exception.RegraNegocioException;
 import org.example.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -22,5 +26,13 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNotFoundExeception (PedidoNaoEncontradoException ex) {
         return new ApiErrors(ex.getMessage());
+    }
+
+    public ApiErrors handleMethodNotValidException (MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map (error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
     }
 }
