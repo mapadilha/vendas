cca95a40e58c4c1d69d7104092773a4f1bbcfe3a
mapadilha <devpadilha@gmail.com>
2024-02-05 10:02:31 -0300

    Configurando a autorização de urls

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -25,6 +25,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                    .authorizeRequests()
+                        .antMatchers("/api/clientes/**")
+                            .authenticated()
+                .and()
+                .formLogin();
+
     }
 }
