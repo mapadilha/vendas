943f30b33e8e70c8e97c38ecaf76962b3fb06fde
mapadilha <devpadilha@gmail.com>
2024-02-05 10:15:57 -0300

    Autenticação Basic

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers("/api/produtos/**")
                             .hasRole("ADMIN")
                          .and()
-                         .formLogin();
+                         .httpBasic();
 
     }
 }
