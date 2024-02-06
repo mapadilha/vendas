9dc990927aa6168670c97c7bf745d3625a112a5f
mapadilha <devpadilha@gmail.com>
2024-02-06 10:26:03 -0300

    Decodificando o token

--- src/main/java/org/example/JwtService.java
+++ src/main/java/org/example/JwtService.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.example.domain.entity.Usuario;
@@ -31,4 +33,31 @@ public class JwtService {
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
     }
+
+    private Claims obterClaims (String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido (String token) {
+        try{
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant()
+                            .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+
 }
